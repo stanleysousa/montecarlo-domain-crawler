0cfe1eb423303b072c6aebb1c75562e4f2789d07
Stanley Sousa <stanleysousa@mail.com>
2024-09-20 00:46:23 -0300

    refactor to more idiomatic F# code

--- app/MCDomainCrawler.Core/Base26.fs
+++ app/MCDomainCrawler.Core/Base26.fs
@@ -1,44 +1,59 @@
 ﻿namespace MCDomainCrawler.Core
 
+open System.Linq
+
 module Base26=
 
-    open System.Linq
+    [<Literal>]
+    let baseValue = 26
 
-    type Digit = { character: char; remainder: int}
+    type Digit =
+        {
+            character: char
+            remainder: int
+        }
 
-    let alphabet = dict [
-        ( '0', 'a' );
-        ( '1', 'b' );
-        ( '2', 'c' );
-        ( '3', 'd' );
-        ( '4', 'e' );
-        ( '5', 'f' );
-        ( '6', 'g' );
-        ( '7', 'h' );
-        ( '8', 'i' );
-        ( '9', 'j' );
-        ( 'A', 'k' );
-        ( 'B', 'l' );
-        ( 'C', 'm' );
-        ( 'D', 'n' );
-        ( 'E', 'o' );
-        ( 'F', 'p' );
-        ( 'G', 'q' );
-        ( 'H', 'r' );
-        ( 'I', 's' );
-        ( 'J', 't' );
-        ( 'K', 'u' );
-        ( 'L', 'v' );
-        ( 'M', 'w' );
-        ( 'N', 'x' );
-        ( 'O', 'y' );
-        ( 'P', 'z' ); ]
+    let alphabet =
+        dict [
+            ( '0', 'a' )
+            ( '1', 'b' )
+            ( '2', 'c' )
+            ( '3', 'd' )
+            ( '4', 'e' )
+            ( '5', 'f' )
+            ( '6', 'g' )
+            ( '7', 'h' )
+            ( '8', 'i' )
+            ( '9', 'j' )
+            ( 'A', 'k' )
+            ( 'B', 'l' )
+            ( 'C', 'm' )
+            ( 'D', 'n' )
+            ( 'E', 'o' )
+            ( 'F', 'p' )
+            ( 'G', 'q' )
+            ( 'H', 'r' )
+            ( 'I', 's' )
+            ( 'J', 't' )
+            ( 'K', 'u' )
+            ( 'L', 'v' )
+            ( 'M', 'w' )
+            ( 'N', 'x' )
+            ( 'O', 'y' )
+            ( 'P', 'z' )
+        ]
 
     let countWords digits=
-        (int)(26.0 ** digits)
+        pown baseValue digits
 
     let getNumber value=
-        { character = alphabet.ElementAt(value % 26).Key; remainder=value/26 }
+        {
+            character = alphabet.ElementAt(value % baseValue).Key; 
+            remainder=value/baseValue
+        }
 
     let getLetter value=
-        { character = alphabet.ElementAt(value % 26).Value; remainder=value/26 }
+        {
+            character = alphabet.ElementAt(value % baseValue).Value;
+            remainder=value/baseValue
+        }
--- app/MCDomainCrawler.Core/RandomNumberGenerator.fs
+++ app/MCDomainCrawler.Core/RandomNumberGenerator.fs
@@ -5,10 +5,34 @@ module RandomNumberGenerator =
     open System
     open MathNet.Numerics
 
-    let createUniformSamples size=
-        let numbersArray:double array = Array.zeroCreate size
-        Distributions.ContinuousUniform.Samples(new Random.MersenneTwister(), numbersArray, 0.0, 1.0)
-        numbersArray
+    let add1 a =
+        a+1
 
-    let createUniformDiscreteSamples size coefficient=
-        createUniformSamples size|> Seq.map(fun number -> (int)(Math.Floor((float)coefficient*number))+1)
+    let nextInt (coefficient : double) (number : double) =
+        coefficient*number
+        |> floor
+        |> int
+        |> add1
+
+    ///<summary>Fills an array with continuous uniform samples.</summary>
+    ///<param name="y">array to be filled.</param>
+    ///<returns>Array filled with samples generated using Mersenne Twister number generator.</returns>
+    let private fillSamples y =
+        Distributions.ContinuousUniform.Samples(Random.MersenneTwister(), y, 0.0, 1.0)
+        y
+
+    ///<summary>Creates samples from a continuous uniform distribution.</summary>
+    ///<param name="n">Number of samples.</param>
+    ///<returns>Samples generated using Mersenne Twister number generator.</returns>
+    let createContinuousSamples n =
+        n
+        |> Array.zeroCreate
+        |> fillSamples
+
+    ///<summary>Creates discrete samples from a uniform distribution.</summary>
+    ///<param name="n">Number of samples.</param>
+    ///<returns>Samples generated using Mersenne Twister number generator.</returns>
+    let createDiscreteSamples n coefficient=
+        n
+        |> createContinuousSamples
+        |> Seq.map(fun n -> nextInt coefficient n)
